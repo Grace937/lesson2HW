d946e6a11bab7f0ae2b171484d94cc564a4f8c7e
Grace937 <87641393+Grace937@users.noreply.github.com>
2021-07-20 16:28:11 +0800

    Add files via upload

---
+++ lesson2HW_1.py
@@ -0,0 +1,6 @@
+score=input('成績?')
+score=int(score)
+if score>=60:
+    print('及格')
+else:
+    print('不及格')
---
+++ lesson2HW_3.py
@@ -0,0 +1,8 @@
+math=int(input('數學成績?'))
+eng=int(input('英文成績?'))
+if math>=90 and eng>=90:
+    print('有獎學金')
+elif math==100 or eng==100:
+    print('有獎學金')
+else:
+    print('沒有獎學金')
